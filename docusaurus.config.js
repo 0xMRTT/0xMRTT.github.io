b68809b8b6a770c42b6e00b3e1ba20110ba7d4c5
0xMRTT <0xMRTT@tuta.io>
2022-05-22 21:50:31 +0200

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '0xMRTT',
-  tagline: 'Ingenieer',
+  tagline: 'Engineer',
   url: 'https://0xMRTT.github.io',
   baseUrl: '/',
   projectName: '0xMRTT.github.io',
