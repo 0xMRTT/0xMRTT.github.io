f68915a361ab9c6398abc382e9f4de71f9d01639
0xMRTT <0xMRTT@tuta.io>
2022-05-22 21:27:19 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,13 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/projects">
+            Projects
+          </Link>
+          <Link
+            className="button button--primary button--lg"
+            to="/about">
+            About
           </Link>
         </div>
       </div>
